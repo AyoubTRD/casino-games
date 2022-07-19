a4a229659527a2eef3ac609995f42d8433c63b55
Ayoub Taouarda <ayoubtrdone@gmail.com>
2022-07-19 01:45:20 +0100

    Refactor and use Shared Module

--- src/app/games/games.module.ts
+++ src/app/games/games.module.ts
@@ -1,21 +1,21 @@
+import { CommonModule } from '@angular/common';
+import { HttpClientModule } from '@angular/common/http';
 import { NgModule } from '@angular/core';
 
+import { HomeComponent } from '../home/home.component';
 import { EmptyStateComponent } from '../shared/components/empty-state/empty-state.component';
 import { GameListComponent } from './game-list/game-list.component';
 import { GameListItemComponent } from './game-list-item/game-list-item.component';
-import { HomeComponent } from '../home/home.component';
 import { TabbarComponent } from './tabbar/tabbar.component';
-import { HttpClientModule } from '@angular/common/http';
-import { CommonModule } from '@angular/common';
+import { SharedModule } from '../shared/shared.module';
 
 @NgModule({
   declarations: [
     GameListComponent,
     TabbarComponent,
     GameListItemComponent,
-    EmptyStateComponent,
     HomeComponent,
   ],
-  imports: [CommonModule, HttpClientModule],
+  imports: [CommonModule, HttpClientModule, SharedModule],
 })
 export class GamesModule {}
--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -1,5 +1,6 @@
-import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { NgModule } from '@angular/core';
+
 import { EmptyStateComponent } from './components/empty-state/empty-state.component';
 
 @NgModule({
