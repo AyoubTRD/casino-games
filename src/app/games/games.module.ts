9e5fa474b342412e59898fdb276ce227b776ef5e
Ayoub Taouarda <ayoubtrdone@gmail.com>
2022-07-19 01:46:22 +0100

    Refactor and use Shared Module

--- src/app/games/games.module.ts
+++ src/app/games/games.module.ts
@@ -3,11 +3,10 @@ import { HttpClientModule } from '@angular/common/http';
 import { NgModule } from '@angular/core';
 
 import { HomeComponent } from '../home/home.component';
-import { EmptyStateComponent } from '../shared/components/empty-state/empty-state.component';
+import { SharedModule } from '../shared/shared.module';
 import { GameListComponent } from './game-list/game-list.component';
 import { GameListItemComponent } from './game-list-item/game-list-item.component';
 import { TabbarComponent } from './tabbar/tabbar.component';
-import { SharedModule } from '../shared/shared.module';
 
 @NgModule({
   declarations: [
