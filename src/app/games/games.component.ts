409df0b442c1c82cbd95012bcc73017482293218
Ayoub Taouarda <ayoubtrdone@gmail.com>
2022-07-19 22:08:36 +0100

    Refactor Games Module, Create Games Routing Module, Lazy load Games Module, and more refactoring

---
+++ src/app/games/games.component.ts
@@ -0,0 +1,67 @@
+import { Component, OnInit, OnChanges, OnDestroy, SimpleChanges } from '@angular/core';
+import { Subscription } from "rxjs"
+
+import { IGame } from "./game";
+import { GamesService } from "./games.service"
+
+@Component({
+  selector: 'app-games',
+  templateUrl: './games.component.html',
+})
+export class GamesComponent implements OnInit, OnChanges, OnDestroy {
+  categories = ['new'];
+
+  showJackpotGamesOnly = false;
+
+  handleCategoriesChange(categories: string[]) {
+    this.showJackpotGamesOnly = false;
+    this.categories = categories;
+  }
+
+  handleShowJackpots() {
+    this.categories = [];
+    this.showJackpotGamesOnly = true;
+  }
+
+  games: IGame[] = [];
+  filteredGames: IGame[] = [];
+
+  isLoading = true;
+  error = false;
+
+  sub!: Subscription;
+
+  constructor(private service: GamesService) { }
+
+  ngOnInit(): void {
+    this.sub = this.service.subscribeToGames().subscribe({
+      next: (games) => {
+        this.games = games;
+        this.isLoading = false;
+        this.filterGames();
+      },
+      error: () => (this.error = true),
+    });
+  }
+
+  filterGames() {
+    if (this.showJackpotGamesOnly) {
+      this.filteredGames = this.games.filter((game) => !!game.jackpotAmount);
+      return;
+    }
+
+    this.filteredGames = this.games.filter((game) =>
+      game.categories.some((category) => this.categories.includes(category))
+    );
+  }
+
+  ngOnChanges(changes: SimpleChanges) {
+    if (changes['categories'] || changes['showJackpotGamesOnly']) {
+      this.filterGames();
+    }
+  }
+
+  ngOnDestroy(): void {
+    this.sub.unsubscribe();
+  }
+}
--- src/constants/api.ts
+++
@@ -1,4 +0,0 @@
-import { environment } from 'src/environments/environment';
-
-export const GAMES_ENDPOINT = environment.backendUrl + '/games.php';
-export const JACKPOTS_ENDPOINT = environment.backendUrl + '/jackpots.php';
