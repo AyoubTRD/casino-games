1a48001ba4cf6bed8c4e81b60af9f93bc97d3666
Ayoub Taouarda <ayoubtrdone@gmail.com>
2022-07-21 20:13:37 +0100

    Add Ribbon component and use it for games

--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -3,11 +3,22 @@ import { NgModule } from '@angular/core';
 
 import { ButtonComponent } from './components/button/button.component';
 import { EmptyStateComponent } from './components/empty-state/empty-state.component';
+import { RibbonComponent } from './components/ribbon/ribbon.component';
 import { SkeletonComponent } from './components/skeleton/skeleton.component';
 
 @NgModule({
-  declarations: [EmptyStateComponent, ButtonComponent, SkeletonComponent],
-  exports: [EmptyStateComponent, ButtonComponent, SkeletonComponent],
+  declarations: [
+    EmptyStateComponent,
+    ButtonComponent,
+    SkeletonComponent,
+    RibbonComponent,
+  ],
+  exports: [
+    EmptyStateComponent,
+    ButtonComponent,
+    SkeletonComponent,
+    RibbonComponent,
+  ],
   imports: [CommonModule],
 })
 export class SharedModule {}
