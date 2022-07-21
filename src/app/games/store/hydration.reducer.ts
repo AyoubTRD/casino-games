7db499b828f78384a9fa44555e3e8448e055f1d0
Ayoub Taouarda <ayoubtrdone@gmail.com>
2022-07-21 21:47:11 +0100

    Add Games Hydration Meta Reducer for keeping the selected categories after refresh

---
+++ src/app/games/store/hydration.reducer.ts
@@ -0,0 +1,42 @@
+import { ActionReducer, INIT, MetaReducer, UPDATE } from '@ngrx/store';
+
+import { GamesState, initialState } from './games.reducer';
+
+export const gamesHydrationMetaReducer: MetaReducer<GamesState> = (
+  reducer: ActionReducer<GamesState>
+) => {
+  return (state, action) => {
+    if (action.type === INIT || action.type === UPDATE) {
+      const params = new URLSearchParams(window.location.search);
+      const categories = params.get('categories');
+      const showJackpotGamesOnly = params.get('showJackpotGamesOnly');
+      return {
+        ...initialState,
+        categories: categories
+          ? categories.split(',')
+          : initialState.categories,
+        showJackpotGamesOnly: showJackpotGamesOnly === 'true',
+      };
+    }
+
+    const nextState = reducer(state, action);
+
+    const params = new URLSearchParams(window.location.search);
+    params.set('categories', nextState.categories.join(','));
+    params.set(
+      'showJackpotGamesOnly',
+      nextState.showJackpotGamesOnly ? 'true' : 'false'
+    );
+
+    const newUrl =
+      window.location.protocol +
+      '//' +
+      window.location.host +
+      window.location.pathname +
+      '?' +
+      params.toString();
+    window.history.pushState({ path: newUrl }, '', newUrl);
+
+    return nextState;
+  };
+};
--- src/app/games/store/index.ts
+++ src/app/games/store/index.ts
@@ -1,7 +1,16 @@
-import { createFeatureSelector, createSelector } from '@ngrx/store';
+import {
+  createFeatureSelector,
+  createSelector,
+  MetaReducer,
+} from '@ngrx/store';
 
 import * as AppState from '../../app.state';
 import { GamesState } from './games.reducer';
+
+export { gamesReducer } from './games.reducer';
+import { gamesHydrationMetaReducer } from './hydration.reducer';
+
+export const metaReducers: MetaReducer[] = [gamesHydrationMetaReducer];
 
 export interface State extends AppState.State {
   games: GamesState;
